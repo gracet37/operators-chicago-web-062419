05efa45c6ef7f5b7106d3cc6baf99ad62e1db969
Grace Tan <gracetan37@gmail.com>
2019-05-25 19:32:07 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,7 +9,7 @@ end
 end
 
 
-#multiple conditions on one line (condition 1 ? )
+#multiple conditions on one line (condition 1 ? ifTrue " condition 2? ifTrue : ifFalse")
 def not_safe?(speed)
 	speed < 40  ? true : speed > 60 ? true : false 
 end
