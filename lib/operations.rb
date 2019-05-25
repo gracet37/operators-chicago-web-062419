8a3170dda00ee15bcdbb450372fb3e7b567dde0f
Grace Tan <gracetan37@gmail.com>
2019-05-25 19:14:26 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
-if speed < 40
-  return "true"
-elsif speed > 60
- return "true"
-else
-  return "false"
+ if speed < 40
+   "true"
+ elsif speed > 60
+   "true"
+ else
+   "false"
 end
 
 
